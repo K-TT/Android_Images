6c3f7052f17b743d919044dc550b18546a4d02ea
Kyleshyro <anthony.rajaofera@gmail.com>
2021-12-13 16:36:01 -0500

    updated internal documentation

---
+++ .history/app/src/main/java/comp_318/group/android_images/MLExecutionViewModel_20211213163356.kt
@@ -0,0 +1,55 @@
+
+/** Group 3: Software Devices 
+ * Image Segmentation
+ * Student name:
+ * Rjaofera Tiana Andriamasinalivao
+ * Burhan Jawed
+ * Kateryna Khomenko
+ * Oksana Koshulap
+ */
+package comp_318.group.android_images
+
+import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.io.File
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.examples.imagesegmentation.tflite.ImageSegmentationModelExecutor
+import org.tensorflow.lite.examples.imagesegmentation.tflite.ModelExecutionResult
+import org.tensorflow.lite.examples.imagesegmentation.utils.ImageUtils
+
+private const val TAG = "MLExecutionViewModel"
+
+class MLExecutionViewModel : ViewModel() {
+
+  private val _resultingBitmap = MutableLiveData<ModelExecutionResult>()
+
+  val resultingBitmap: LiveData<ModelExecutionResult>
+    get() = _resultingBitmap
+
+  private val viewModelJob = Job()
+  private val viewModelScope = CoroutineScope(viewModelJob)
+
+  // the execution of the model has to be on the same thread where the interpreter
+  // was created
+  fun onApplyModel(
+    filePath: String,
+    imageSegmentationModel: ImageSegmentationModelExecutor?,
+    inferenceThread: ExecutorCoroutineDispatcher
+  ) {
+    viewModelScope.launch(inferenceThread) {
+      val contentImage = ImageUtils.decodeBitmap(File(filePath))
+      try {
+        val result = imageSegmentationModel?.execute(contentImage)
+        _resultingBitmap.postValue(result)
+      } catch (e: Exception) {
+        Log.e(TAG, "Fail to execute ImageSegmentationModelExecutor: ${e.message}")
+        _resultingBitmap.postValue(null)
+      }
+    }
+  }
+}
